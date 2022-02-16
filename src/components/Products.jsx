f346d0b87d0c51b3395d19bb0a81f9e8af5c6783
papijo <joe.ebh100@live.com>
2022-02-16 13:34:32 +0100

    changed url in products component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Product from "./Product";
-import axios from "axios";
+import { publicRequest } from "../requestMethods";
 
 const Container = styled.div`
   padding: 20px;
@@ -17,10 +17,8 @@ const Products = ({ cat, filters, sort }) => {
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const res = await axios.get(
-          cat
-            ? `http://localhost:3001/api/products?category=${cat}`
-            : "http://localhost:3001/api/products"
+        const res = await publicRequest.get(
+          cat ? `products?category=${cat}` : "products"
         );
         setProducts(res.data);
         // console.log(res.data);
