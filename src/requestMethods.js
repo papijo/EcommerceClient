5f4bbcbceccc3998c704adddeae57bee07ce33b4
Ebhota Jonathan Junior Edor <43897703+papijo@users.noreply.github.com>
2022-02-16 11:35:46 +0100

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,14 +3,14 @@ import axios from "axios";
 // const BASE_URL = "http://localhost:3001/api/";
 const BASE_URL = "https://ecomestoreapi.herokuapp.com/api/";
 
-const TOKEN =
-  JSON?.parse(JSON?.parse(localStorage?.getItem("persist:root"))?.user)
-    ?.currentUser?.accessToken || "";
+// const TOKEN =
+//   JSON?.parse(JSON?.parse(localStorage?.getItem("persist:root"))?.user)
+//     ?.currentUser?.accessToken || "";
 
 // console.log(TOKEN);
 
-// const TOKEN =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTA3OGM0YTBjYWEwM2JmNGU2NWNiMyIsImlzU3RhZmYiOnRydWUsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NDYxMjk4MSwiZXhwIjoxNjQ0ODcyMTgxfQ.Fked8Am0oCcHVx4Fy44HUQv7LoXdCg70vlyMDm38tqM";
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTA3OGM0YTBjYWEwM2JmNGU2NWNiMyIsImlzU3RhZmYiOnRydWUsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NTAwNzY0M30.cOfKVsiKTL4IAVWCmqPRmQPlNm6vzKnhX7gI_7rUd1s";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
